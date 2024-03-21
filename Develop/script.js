90a64872c74428b3db7e74950770cb4133501e77
Bryan O <bocampo91@yahoo.com>
2024-03-21 13:18:19 -0400

    Initial commit

---
+++ Develop/script.js
@@ -0,0 +1,123 @@
+// Get a reference to the #add-employees-btn element
+const addEmployeesBtn = document.querySelector('#add-employees-btn');
+let employeesArray = [];
+
+// Collect employee data
+const collectEmployees = function () {
+  // TODO: Get user input to create and return an array of employee objects
+
+  //Create flag if user wants to add another employee to table
+  let anotherEmployee = true;
+
+  //Create While loop for employee entry under user no longer wants to add more employees
+  while (anotherEmployee) {
+    employeesArray.push({
+      firstName: prompt("Please enter your first name."),
+      lastName: prompt("Please enter your last name."),
+
+      //Convert entered string from prompt to an integer
+      salary: parseInt(prompt("What is your salary?"))
+    });
+
+    if (!confirm("Add another employee?")) {
+
+      //When user selects cancel to stop adding employees, the employeesArray array is returned with all added entries
+      anotherEmployee = false;
+      return employeesArray;
+    }
+  }
+}
+
+
+// Display the average salary
+const displayAverageSalary = function (employeesArray) {
+  // TODO: Calculate and display the average salary
+
+  //Initialize sum with 0 value
+  let sum = 0;
+
+  //Create For loop in order to add each employee's salary in the tablet to the sum variable
+  for (let i = 0; i < employeesArray.length; i++) {
+    sum += Number(employeesArray[i].salary);
+  }
+
+  //Returns average of employee salaries
+  return console.log("Average Salary: " + (sum / employeesArray.length));
+}
+
+// Select a random employee
+const getRandomEmployee = function (employeesArray) {
+  // TODO: Select and display a random employee
+
+  //Create random number
+  let selectEmployee = Math.floor(Math.random() * employeesArray.length);
+
+  //Return employee first & last name that matches the index of the random number that was generated
+  return console.log(`Congratulations ${employeesArray[selectEmployee].firstName} ${employeesArray[selectEmployee].lastName}, our random drawing winner!`);
+}
+
+/*
+  ====================
+  STARTER CODE
+  Do not modify any of the code below this line:
+*/
+
+// Display employee data in an HTML table
+const displayEmployees = function (employeesArray) {
+  // Get the employee table
+  const employeeTable = document.querySelector('#employee-table');
+
+  // Clear the employee table
+  employeeTable.innerHTML = '';
+
+  // Loop through the employee data and create a row for each employee
+  for (let i = 0; i < employeesArray.length; i++) {
+    const currentEmployee = employeesArray[i];
+
+    const newTableRow = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = currentEmployee.firstName;
+    newTableRow.append(firstNameCell);
+
+    const lastNameCell = document.createElement("td");
+    lastNameCell.textContent = currentEmployee.lastName;
+    newTableRow.append(lastNameCell);
+
+    const salaryCell = document.createElement("td");
+    // Format the salary as currency
+    salaryCell.textContent = currentEmployee.salary.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD"
+    });
+
+    newTableRow.append(salaryCell);
+
+    employeeTable.append(newTableRow);
+  }
+}
+
+const trackEmployeeData = function () {
+  const employees = collectEmployees();
+
+  console.table(employees);
+
+  displayAverageSalary(employees);
+
+  console.log('==============================');
+
+  getRandomEmployee(employees);
+
+  employees.sort(function (a, b) {
+    if (a.lastName < b.lastName) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  displayEmployees(employees);
+}
+
+// Add event listener to 'Add Employees' button
+addEmployeesBtn.addEventListener('click', trackEmployeeData);
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Challenge-03
+I created an Employee Payroll Tracker using the following criteria:
+
+GIVEN an employee payroll tracker
+WHEN I click the "Add employee" button
+THEN I am presented with a series of prompts asking for first name, last name, and salary
+WHEN I finish adding an employee
+THEN I am prompted to continue or cancel
+WHEN I choose to continue
+THEN I am prompted to add a new employee
+WHEN I choose to cancel
+THEN my employee data is displayed on the page sorted alphabetically by last name, and the console shows computed and aggregated data
+```
+
+I also edited the displayAverageSalary() and getRandomEmployee() functions to do the following:
+
+WHEN I as the user no longer wish to add more employees to the table
+THEN I am presented with the average of employee salaries
+WHEN the average salary is calculated and logged to the console
+THEN I am presented with the employee's first and last name who won the random draw selection in the console
